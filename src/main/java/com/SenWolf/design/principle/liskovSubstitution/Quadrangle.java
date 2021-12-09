53339f6878503b09decf82bf0dc47c8304b26180
Kris <krischuc@163.com>
2021-12-09 23:28:10 +0800

    [Study]:Design Pattern Study 6. Liskov Substitution Principle

---
+++ src/main/java/com/SenWolf/design/principle/liskovSubstitution/Quadrangle.java
@@ -0,0 +1,10 @@
+package com.SenWolf.design.principle.liskovSubstitution;
+
+/**
+ * @author SenWolf
+ * @date 2021/12/9 23:20
+ */
+public interface Quadrangle {
+    long getWidth();
+    long getLength();
+}
