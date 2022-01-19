4b3717d1905dfaffcbc5166949b8b2654c241788
Kris <krischuc@163.com>
2022-01-19 21:16:20 +0800

    [Study]:Design Pattern Study 1. builder

---
+++ src/main/java/com/SenWolf/design/pattern/creational/builder/Coach.java
@@ -0,0 +1,24 @@
+package com.SenWolf.design.pattern.creational.builder;
+
+/**
+ * @author SenWolf
+ * @date 2022/1/19 20:36
+ */
+public class Coach {
+    private CourseBuilder courseBuilder;
+
+    public void setCourseBuilder(CourseBuilder courseBuilder) {
+        this.courseBuilder = courseBuilder;
+    }
+
+    public Course makeCourse(String courseName, String coursePPT,
+                             String courseVideo, String courseArticle,
+                             String courseQA) {
+        this.courseBuilder.buildCourseName(courseName);
+        this.courseBuilder.buildCoursePPT(courseName);
+        this.courseBuilder.buildCourseVideo(courseName);
+        this.courseBuilder.buildCourseArticle(courseName);
+        this.courseBuilder.buildCourseQA(courseName);
+        return this.courseBuilder.makeCourse();
+    }
+}
